075678aee9701f2b5ab8dee266bc166aba934246
Luris993 <lukasztopa1993@gmail.com>
2019-05-19 15:10:01 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ const elementHeader = document.querySelector('#main_header');
 const aboutButton = elementHeader.querySelector('.nav .about_btn');
 const recommendationsButton = elementHeader.querySelector('.nav .recommendations_btn');
 const teamButton = elementHeader.querySelector('.nav .team_btn');
+const contactButton = elementHeader.querySelector('.nav .contact_btn');
 const footer = document.querySelector('.page_footer');
 
 
@@ -43,6 +44,22 @@ teamButton.addEventListener('click', function(e) {
 	e.preventDefault();
 	
 	const href = 'team.html';
+	
+	footer.classList.remove('bounceInUp');
+	footer.classList.add('bounceOutDown');	
+	
+	elementHeader.classList.remove('bounceInLeft');
+	elementHeader.classList.add('bounceOutLeft');
+	
+	elementHeader.addEventListener('animationend', function() {
+		window.location = href;
+	})
+});
+
+contactButton.addEventListener('click', function(e) { 
+	e.preventDefault();
+	
+	const href = 'contact.html';
 	
 	footer.classList.remove('bounceInUp');
 	footer.classList.add('bounceOutDown');	
