e7b767a0b45fa12338725e133cf950e9cf86183e
Luris993 <lukasztopa1993@gmail.com>
2019-06-12 21:06:15 +0200

    edit responsive page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,9 @@ const footer = document.querySelector('.page_footer');
 const toggleBtn = elementHeader.querySelector('.toggle_btn');
 const heroContainer = elementHeader.querySelector('.hero_container');
 const nav = elementHeader.querySelector('.nav');
+const logo = elementHeader.querySelector('.hero_logo');
+const fa_bars = toggleBtn.querySelector('.fa-bars');
+const fa_times = toggleBtn.querySelector('.fa-times');
 
 aboutButton.addEventListener('click', function(e) { 
 	e.preventDefault();
@@ -75,8 +78,19 @@ contactButton.addEventListener('click', function(e) {
 
 toggleBtn.addEventListener('click', function() {
 
+	fa_bars.classList.toggle('btn_open');
+	fa_times.classList.toggle('btn_close');
 	heroContainer.classList.toggle('hero_container_toggle');
 	nav.classList.toggle('nav_close');
 	nav.classList.toggle('nav_open');
 
+});
+
+logo.addEventListener('click', function(){
+
+	fa_bars.classList.toggle('btn_open');
+	fa_times.classList.toggle('btn_close');
+	heroContainer.classList.toggle('hero_container_toggle');
+	nav.classList.toggle('nav_close');
+	nav.classList.toggle('nav_open');
 });
